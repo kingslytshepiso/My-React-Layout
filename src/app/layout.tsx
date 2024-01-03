d5fe7e56f138b22b6982390056c7064a4ed4befa
Kingsly Mokgwathi <s220191506@mandela.ac.za>
2024-01-03 06:04:16 +0200

    repo name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,43 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './globals.css'
-import Header from './header'
-import Sidebar from './sidebar'
-import Footer from './footer'
-import { useEffect } from 'react';
-import ImportBsJs from './importBsJs';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
+import Header from "./header";
+import Sidebar from "./sidebar";
+import Footer from "./footer";
+import { useEffect } from "react";
+import ImportBsJs from "./importBsJs";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'layout',
-  description: 'my react layout design',
-}
+  title: "layout",
+  description: "my react layout design",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
       <body>
-        <header><Header /></header>
-        <main className='container-fluid'>
-          <div className='d-sm-block d-none d-md-block'><Sidebar /></div>
-          <div className='p-3'>{children}</div>
+        <header>
+          <Header />
+        </header>
+        <main className="container-fluid">
+          <div className="d-sm-block d-none d-md-block">
+            <Sidebar />
+          </div>
+          <div className="p-3">{children}</div>
         </main>
-        <footer><Footer /></footer>
+
+        <footer>
+          <Footer />
+        </footer>
         <ImportBsJs />
       </body>
     </html>
-  )
+  );
 }
